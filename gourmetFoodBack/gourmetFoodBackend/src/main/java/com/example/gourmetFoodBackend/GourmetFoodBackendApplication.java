ea9f1893f3a31cad226bdaa443bd94d505fe17b8
mdevon19 <57203156+mdevon19@users.noreply.github.com>
2021-07-26 10:56:58 -0400

    Add files via upload

---
+++ gourmetFoodBack/gourmetFoodBackend/src/main/java/com/example/gourmetFoodBackend/GourmetFoodBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gourmetFoodBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GourmetFoodBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GourmetFoodBackendApplication.class, args);
+	}
+
+}
---
+++ gourmetFoodBack/gourmetFoodBackend/src/test/java/com/example/gourmetFoodBackend/GourmetFoodBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gourmetFoodBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmetFoodBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
